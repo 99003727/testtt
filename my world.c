0e59fc46974171950650be520ac0a31192336aa1
99003727 <78849172+99003727@users.noreply.github.com>
2021-02-14 11:20:51 +0530

    changed the code

--- my world.c
+++ my world.c
@@ -4,18 +4,18 @@ int main()
 {
     //char a[20]="";
     float degree,rad;
-    printf("what do you want convert?give the choice as D or R");
-    char b=;
-    scanf("%c",&b);
-    if(b=="D")
+    printf("what do you want convert?give the choice as 1Degree. or 2.Radian\n");
+    char b[10];
+    scanf("%c",b);
+    if(b[0]="D")
     {
-    printf("give the data to convert from degrees to radian");
+    printf("give the data to convert from degrees to radian\n");
     scanf("%f",&degree);
     rad=degree*(M_PI/180);
     printf("%f", rad);}
     else
     {
-        printf("give the data to convert from radian to degrees");
+        printf("give the data to convert from radian to degrees\n");
         scanf("%f",&rad);
         degree=rad/(M_PI/180);
         printf("%f",degree);
